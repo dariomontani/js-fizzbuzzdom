db152d4c1c20f7a521533443eba7bdb338dd4e9a
dariomontani <dariomontani@gmail.com>
2021-11-23 16:42:29 +0100

    correction else if

--- js/script.js
+++ js/script.js
@@ -11,16 +11,16 @@
 
 // ciclo da 1 a 100
 for (let i = 1; i < 101; i++ ){
-    if ((i % 3 == 0) && (!(i % 5 == 0))) {
+    if (i % 3 == 0) {
         console.log('Fizz');
     }
-    if ((i % 5 == 0) && (!(i % 3 == 0))) {
+    else if (i % 5 == 0) {
         console.log('Buzz');
     }
-    if ((i % 3 == 0) && (i % 5 == 0)) {
+    else if ((i % 3 == 0) && (i % 5 == 0)) {
         console.log('FizzBuzz');
     }
-    if ((!(i % 3 == 0)) && (!(i % 5 == 0))) {
+    else {
         console.log(i);
     }
 }
